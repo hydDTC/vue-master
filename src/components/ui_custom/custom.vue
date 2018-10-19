503b915e6940f3da53c61d30e1b8613a28fe5b80
胡誉娣 <huyudi@ycmedia.cn>
2018-10-19 17:59:24 +0800

    youhua

---
+++ src/components/ui_custom/custom.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div id="tenant">
+         我是customer{{num}}
+        <customer-child :num="num" :obj="obj"></customer-child>
+    </div>
+
+</template>
+
+<script>
+
+
+    import customerChild from './customer-child.vue';
+
+    export default{
+        data(){
+            return {
+                num: 20,
+                obj: {
+                    name: 'hyd',
+                    age: 30
+                }
+            }
+        },
+        created(){
+            console.log("页面点进")
+        },
+        // 要注册组件哈
+        components:{
+            customerChild
+        },
+        methods:{
+
+        }
+    }
+</script>
---
+++ src/components/ui_custom/customer-child.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div>
+        <hr/>
+        {{title}}
+        父亲传过来的年龄：{{num}}
+        父亲传过来的对象: {{obj.name}}
+    </div>
+
+</template>
+
+
+
+<script>
+
+    export default {
+        name:'',
+        props: ["num","obj"],
+        data(){
+            return {
+                // num:'', // 要记得初始化定义的
+                title:'我是customer的子组件'
+            }
+        },
+        'methods': {
+
+        }
+    }
+</script>
